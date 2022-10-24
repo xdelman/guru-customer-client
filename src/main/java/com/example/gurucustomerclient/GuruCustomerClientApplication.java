4b15f5cb33e73558ecbcb9945bb72a63ecc0ad47
XolaniDelman <Xolani.Delman@Absa.africa>
2022-10-24 14:27:51 +0200

    Initial commit

---
+++ src/main/java/com/example/gurucustomerclient/GuruCustomerClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.gurucustomerclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuruCustomerClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuruCustomerClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gurucustomerclient/GuruCustomerClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gurucustomerclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuruCustomerClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
